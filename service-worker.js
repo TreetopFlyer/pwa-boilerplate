696c1ad4d951e97f34992d1ff330f14bd44b589d
unknown <TreetopFlyer>
2019-01-20 08:29:34 -0500

    workbox added

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,37 @@
-self.addEventListener('install', function(event)
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox)
 {
-    caches
-    .open("v1")
-    .then(function(inCache)
-    {
-        return inCache.addAll([
-            "/.",
-            "/index.html"
-        ]);
-    })
-    .then(function()
-    {
-        console.log("installed and cache added");
-    });
-});
+    workbox.precaching.precacheAndRoute([
+        '/index.html',
+    ]);
 
-self.addEventListener('activate', function(event) {
-    console.log("activated");
-});
+    workbox.routing.registerRoute(
+        '//api.boxcast.com/channels/zqpjrxasjzj9eugdcdxg/broadcasts',
+        workbox.strategies.cacheFirst(
+        {
+            plugins: [
+                new workbox.cacheableResponse.Plugin(
+                {
+                    statuses: [0, 200]
+                })
+            ]
+        }),
+    );
 
-self.addEventListener('fetch', function(event) {
-    console.log(event.request);
-});
+    workbox.routing.registerRoute(
+        /.*\.(?:png|jpg|jpeg|svg|gif)/,
+        workbox.strategies.cacheFirst(
+        {
+            cacheName: 'images',
+            plugins: [
+                new workbox.expiration.Plugin(
+                {
+                    maxEntries: 20,
+                    maxAgeSeconds: 2 * 60 * 60,
+                })
+            ],
+        })
+    );
+
+}
